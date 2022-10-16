9cba6cf2c8cb789114a72799067d2ae2576d069e
admin <admin@DESKTOP-OOLGVSP>
2022-10-16 19:01:18 +0530

    done

---
+++ firstproject/src/firstproject/scope.java
@@ -0,0 +1,18 @@
+package firstproject;
+
+public class scope {
+
+	public static void main(String[] args) {
+		int outer=3;
+		{
+			int inner=40;
+			System.out.println(inner);
+			System.out.println(outer);
+		}
+		int inner=3;
+		System.out.println(inner);
+		// TODO Auto-generated method stub
+
+	}
+
+}
