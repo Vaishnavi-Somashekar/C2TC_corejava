c0f8450aa00e8c53e8fff65c753b98ea3c7d9ab8
admin <admin@DESKTOP-OOLGVSP>
2022-10-16 19:23:55 +0530

    ifExample is done

---
+++ firstproject/src/firstproject/ifExample.java
@@ -0,0 +1,20 @@
+package firstproject;
+
+public class ifExample {
+
+	public static void main(String[] args) {
+		int a=2;
+		if(a==1) {
+			System.out.println("one");
+		}
+		else if(a==2) {
+			System.out.println("two");
+		}
+		else {
+			System.out.println("invalid");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
