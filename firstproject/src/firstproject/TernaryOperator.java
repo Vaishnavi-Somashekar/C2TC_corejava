f2c571cb453df78462cca48d832e382339e2c343
admin <admin@DESKTOP-OOLGVSP>
2022-10-16 19:36:32 +0530

    TernaryOperator is done

---
+++ firstproject/src/firstproject/TernaryOperator.java
@@ -0,0 +1,16 @@
+package firstproject;
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+		int v=1;
+		System.out.println(v==1 ?"Hello":"Word");
+		v++;
+		System.out.println(v==1 ?"Hello":"World");
+
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
