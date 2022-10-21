b4a57e713ed85c630a4d3845640729f8655169b6
admin <admin@DESKTOP-OOLGVSP>
2022-10-21 19:20:33 +0530

    first commit

---
+++ Constructor/src/Employee.java
@@ -0,0 +1,11 @@
+class Employee {
+	int id;
+	String name;
+	float salary;
+	Employee() {
+		System.out.println("user defined no-arguments" + "constructor executed");
+	}
+void display() {
+	System.out.println(id + " " + name + " " + salary);
+    }
+}
