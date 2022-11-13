3486a59aaa98558dca3db0b7996d0d838b122028
admin <admin@DESKTOP-OOLGVSP>
2022-11-13 19:32:15 +0530

    done

---
+++ Controlstatements/src/Language.java
@@ -0,0 +1,13 @@
+public class Language {
+
+	public static void main(String[] args) {
+		String lang ="java";
+		if(lang =="python") {
+			System.out.println("language is python");
+		}else if (lang  == "C++") {
+			System.out.println("lang is C++");
+		}else if(lang =="java") {
+			System.out.println("lang is java");
+		}else {
+			System.out.println("lang is java"); }
+	}}
