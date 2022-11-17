bb12da14ba6046dc371d0f0116e8f668d498cb2e
admin <admin@DESKTOP-OOLGVSP>
2022-11-17 17:24:09 +0530

    first commit

---
+++ keyword/src/keyword/hello.java
@@ -0,0 +1,25 @@
+class hello
+{
+  hello()
+  {
+	  this(5);
+	  System.out.println("hello a");
+  }
+  hello(int x)
+  {
+	  this();
+	  System.out.println(x);
+	  
+  }
+}
+
+class Main
+
+	public static void main(String args[])
+{
+		hello a=new hello();
+		
+
+	}
+
+}
