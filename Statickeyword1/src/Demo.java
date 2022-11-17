a5f525448e3b238f91f30291ca244f8636e9fad4
admin <admin@DESKTOP-OOLGVSP>
2022-11-17 16:34:24 +0530

    first commit

---
+++ Statickeyword1/src/Demo.java
@@ -0,0 +1,14 @@
+class Demo {
+
+
+		static
+		{
+			System.out.println("static block is invoked");
+		}
+		public static void main(String[] args) {
+			System.out.println("Hello main");
+
+
+	}
+
+}
