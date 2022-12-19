64a9fcf7aa14fe36eb85b5527d64671845463f3e
admin <admin@DESKTOP-OOLGVSP>
2022-12-19 19:43:44 +0530

    test case 2

---
+++ GoShoping/src/com/tns/client/GSClient.java
@@ -0,0 +1,14 @@
+package com.tns.client;
+import com.tns.application.GSNormalAcc;
+import com.tns.application.GSPrimeACC;
+import com.tns.application.GSShopFactory;
+public class GSClient {
+	public static void main(String[] args) {
+		GSShopFactory factory=new GSShopFactory();
+		GSNormalAcc gsn=(GSNormalAcc)factory.getNewNormalAccount(1212, "Vasudeva", 600.00f);
+		gsn.bookProduct(gsn.getCharges());
+		GSPrimeACC gsp=(GSPrimeACC) factory.getNewPrimeAccount(12122, "Ravindra", 500.00f, true);
+		gsp.bookProduct(gsp.getCharges());
+	}
+
+}
