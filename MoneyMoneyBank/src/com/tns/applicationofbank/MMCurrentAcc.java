b2f1e860a9da47bd5172b0117d7887414d91bc2a
admin <admin@DESKTOP-OOLGVSP>
2022-12-19 19:39:26 +0530

    test case 1

---
+++ MoneyMoneyBank/src/com/tns/applicationofbank/MMCurrentAcc.java
@@ -0,0 +1,23 @@
+package com.tns.applicationofbank;
+import com.tns.framework.CurrentAcc;
+
+public class MMCurrentAcc extends CurrentAcc{
+	public MMCurrentAcc(int accNo, String accNm, float accBal) {
+		super(accNo, accNm, accBal);
+		
+	}
+	public void withdraw(float accBal) {
+		System.out.println("Dear Current User,Your account Balance is="+" "+accBal+" "+"and credit limit is="+" "+getcreditLimit());
+	}
+
+	@Override
+	public String toString() {
+		return "MMCurrentAcc [getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()="
+				+ super.toString() + "]";
+	}
+	public float getaccBal() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ MoneyMoneyBank/src/com/tns/applicationofbank/MMSavingAcc.java
@@ -0,0 +1,22 @@
+package com.tns.applicationofbank;
+import com.tns.framework.SavingAcc;
+
+public class MMSavingAcc extends SavingAcc {
+	public MMSavingAcc(int accNo, String accNm, float accBal, boolean isSalary) 
+	{
+		super(accNo, accNm, accBal, isSalary);
+	}
+	public void withdraw(float accBal) {
+		System.out.println("Dear Saving User,your Account Balance is="+" "+accBal+" "+"and credit limit is="+" "+getcreditLimit());
+	}
+
+	@Override
+	public String toString() {
+		return "MMSavingAcc [getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()="
+				+ super.toString() + "]";
+	}
+	public float getaccBal() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
